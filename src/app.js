a24e03de41069019d24a9c9b0db4b605033568d4
rohitbharti279 <rohitbharti279@gmail.com>
2021-07-07 19:57:53 +0530

    basecode

---
+++ src/app.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let stream = require('./ws/stream');
+let path = require('path');
+let favicon = require('serve-favicon');
+
+//set static folder
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');  //we have to send file from HTML
+});
+
+
+io.of('/stream').on('connection', stream);
+
+server.listen(3000);
